7a5054269a733ffdd25359b7b85a9d36c23fc811
DevMias <jakub.mijas2001@gmail.com>
2024-05-30 15:59:52 +0200

    Dockerfiles and docker-compose.yml is added

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
+const appId = process.env.APPID || 'cd48225572f420a5d0619a255c3488f4';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
